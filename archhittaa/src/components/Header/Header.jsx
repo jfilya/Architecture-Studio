ba0a738164230ead4933555a10397630bd312cc9
Julia Filippova <julia.94@bk.ru>
2023-01-02 13:00:19 +0300

    feat: start project

---
+++ archhittaa/src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import style from "./Header.module.scss";
+function Header(){
+    return(
+        <header className={style.header}>
+            <div className={style.header__wrapper} >
+                <div className="container">
+
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header;
