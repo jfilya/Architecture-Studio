eab4385068af198cea755536c13c75cc763285ce
Julia Filippova <julia.94@bk.ru>
2023-01-05 17:09:03 +0300

    feat: add section lataste blogs

--- archhittaa/src/App.js
+++ archhittaa/src/App.js
@@ -1,14 +1,16 @@
-import Header from "./components/Header/Header"
-import Our from "./components/Our/Our"
-import Selected from "./components/Selected/Selected"
-import About from "./components/About/About"
+import Header from "./components/Header/Header";
+import Our from "./components/Our/Our";
+import Selected from "./components/Selected/Selected";
+import About from "./components/About/About";
+import Blogs from "./components/Blogs/Blogs";
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Our/>
-      <Selected/>
-      <About/>
+      <Header />
+      <Our />
+      <Selected />
+      <About />
+      <Blogs />
     </div>
   );
 }
---
+++ archhittaa/src/components/Blogs/Blogs.jsx
@@ -0,0 +1,41 @@
+import style from "./Blogs.module.scss";
+function Blogs() {
+  const blogs = [
+    {
+      img: "images/blog1.jpg",
+      description: "Amet eu facilisi posuere ut at cras non ipsum proin",
+    },
+    {
+      img: "images/blog2.jpg",
+      description: "Emu facilisi posuere ut at cras non ipsum proin",
+    },
+    {
+      img: "images/blog3.jpg",
+      description: "Fasacilisi posuere ut at cras non ipsum proin",
+    },
+  ];
+  return (
+    <section className={style.blogs}>
+      <div className="container">
+        <div className={style.blogs__background}>
+          <div className={style.blogs__block}>
+            <h2>
+              <span></span>Lataste Blogs
+            </h2>
+
+          </div>
+          <div className={style.blogs__items}>
+              {blogs.map((icon, index) => (
+                <div className={style.blogs__item} key={index}>
+                  <span>Intirior</span>
+                  <img src={icon.img} alt="icon" />
+                  <p>{icon.description}</p>
+                </div>
+              ))}
+            </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+export default Blogs;
